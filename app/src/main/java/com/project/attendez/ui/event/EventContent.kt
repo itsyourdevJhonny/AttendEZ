361378e4f85501dec6d5cbad17ca35895887e176
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-05 17:35:02 +0800

    Removed unused combinedClickable import

--- app/src/main/java/com/project/attendez/ui/event/EventContent.kt
+++ app/src/main/java/com/project/attendez/ui/event/EventContent.kt
@@ -6,7 +6,6 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
