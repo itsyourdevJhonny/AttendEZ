1ffd363e3d0c86cd5afe6eb2d0d961f78403ca01
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-03-22 23:38:19 +0800

    feat: Add navigation routes
    This commit introduces the `Routes` sealed class to define navigation paths for the Event, Attendee, Attendance, and History screens.

---
+++ app/src/main/java/com/project/attendez/navigation/Routes.kt
@@ -0,0 +1,8 @@
+package com.project.attendez.navigation
+
+sealed class Routes(val route: String) {
+    object Event : Routes("event")
+    object Attendee: Routes("attendee")
+    object Attendance : Routes("attendance")
+    object History : Routes("history")
+}
