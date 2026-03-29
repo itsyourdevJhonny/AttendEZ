8b9ce6e1905824795ceb7afe94cba9afb15c1c19
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-03-29 19:27:24 +0800

    Remove `date` from `AttendanceEntity` primary keys
    This commit updates the `AttendanceEntity` to use a composite primary key consisting only of `eventId` and `attendeeId`, removing the `date` field.

--- app/src/main/java/com/project/attendez/data/local/entity/AttendanceEntity.kt
+++ app/src/main/java/com/project/attendez/data/local/entity/AttendanceEntity.kt
@@ -8,7 +8,7 @@ import java.time.LocalDateTime
 
 @Entity(
     tableName = "attendance",
-    primaryKeys = ["eventId", "attendeeId", "date"],
+    primaryKeys = ["eventId", "attendeeId"],
     foreignKeys = [
         ForeignKey(
             entity = EventEntity::class,
