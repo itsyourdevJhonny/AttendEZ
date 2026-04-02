cfdc75631816aab880b9030c685369fd78688a62
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-04-02 22:26:54 +0800

    Add `getByStudentId` and `getByEventIdAndDate` to `AttendeeRepository`
    This commit adds two new methods to `AttendeeRepository`: `getByStudentId` for fetching attendees by student ID, and `getByEventIdAndDate` for retrieving attendees for a specific event within a date range.

--- app/src/main/java/com/project/attendez/data/local/repository/AttendeeRepository.kt
+++ app/src/main/java/com/project/attendez/data/local/repository/AttendeeRepository.kt
@@ -3,14 +3,20 @@ package com.project.attendez.data.local.repository
 import com.project.attendez.data.local.dao.AttendeeDao
 import com.project.attendez.data.local.entity.AttendeeEntity
 import kotlinx.coroutines.flow.Flow
+import java.time.LocalDateTime
 import javax.inject.Inject
 
 class AttendeeRepository @Inject constructor(
-    private val attendeeDao: AttendeeDao
+    private val attendeeDao: AttendeeDao,
 ) {
     fun getAttendees(): Flow<List<AttendeeEntity>> = attendeeDao.getAll()
 
     fun getAttendeeById(id: Long): Flow<AttendeeEntity> = attendeeDao.getById(id)
 
     fun getByEventId(eventId: Long) = attendeeDao.getByEventId(eventId)
+
+    fun getByStudentId(studentId: String) = attendeeDao.getByStudentId(studentId)
+
+    fun getByEventIdAndDate(eventId: Long, startOfDay: LocalDateTime, endOfDay: LocalDateTime) =
+        attendeeDao.getByEventIdAndDate(eventId, startOfDay, endOfDay)
 }
