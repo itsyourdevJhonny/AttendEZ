cba72dbfc5b1fff46ad84b21de72c994b7ddc1d6
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-04-15 17:47:14 +0800

    Update `date` type in `AttendanceWithAttendeeRaw.kt`
    This commit changes the type of the `date` field in the `AttendanceWithAttendeeRaw` data class from `LocalDate` to `LocalDateTime`.

--- app/src/main/java/com/project/attendez/data/local/util/AttendanceWithAttendeeRaw.kt
+++ app/src/main/java/com/project/attendez/data/local/util/AttendanceWithAttendeeRaw.kt
@@ -2,13 +2,14 @@ package com.project.attendez.data.local.util
 
 import com.project.attendez.data.local.entity.AttendanceStatus
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 data class AttendanceWithAttendeeRaw(
     // Attendance fields
     val eventId: Long,
     val attendeeId: Long,
     val status: AttendanceStatus,
-    val date: LocalDate,
+    val date: LocalDateTime,
     val markedAt: Long,
 
     // Attendee fields
