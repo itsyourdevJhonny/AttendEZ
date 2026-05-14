0c1daab520c600fe401a82d4b14e56592084ce0b
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-05-14 21:58:59 +0800

    Add `google-services` plugin to `build.gradle.kts`
    This commit adds the `com.google.gms.google-services` plugin version `4.4.4` to the project's root `build.gradle.kts` file with `apply false`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
 
     id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
     id("com.google.dagger.hilt.android") version "2.50" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
